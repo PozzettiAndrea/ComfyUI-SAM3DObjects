496371b1ff0e7db21e2417e35ae4b5b62358c0a1
PozzettiAndrea <andrea@pozzetti.it>
2026-02-09 19:16:05 +0100

    modernised

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
-from comfy_env import wrap_nodes
-from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-
-wrap_nodes()
+import sys
+print("[sam3dobjects] loading...", file=sys.stderr, flush=True)
+from comfy_env import register_nodes
+print("[sam3dobjects] calling register_nodes", file=sys.stderr, flush=True)
+NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS = register_nodes()
 
 WEB_DIRECTORY = "./web"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
